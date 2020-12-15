55dc1ab441b37f32f00356bdd7149e3cbc8188fe
Florian Uhde <floar@mx.de>
2020-12-15 12:54:49 +0100

    done flo

---
+++ Assets/Fluhde/Rotate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+
+    public float RotateSpeed = 1f;
+    public float FloatSpeed = 1f;
+    public float FloatScale = 1f;
+
+    private Vector3 _basePos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _basePos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up, RotateSpeed * Time.deltaTime, Space.World);
+        transform.position = _basePos + FloatScale* Vector3.up * Mathf.Sin(Time.time * FloatSpeed);
+
+    }
+}
